e2a5c258b8acd0930dbe985b10911ca674f77dfb
Mufeez Ahmad <md.coding2k22@gmail.com>
2025-10-03 02:27:17 +0500

    Delete csharp/FlashTokenizer.Benchmarks/bin/Release/net8.0 directory

--- csharp/FlashTokenizer.Benchmarks/bin/Release/net8.0/sample/sample_bpe.py
+++
@@ -1,8 +0,0 @@
-from transformers import GPT2TokenizerFast
-
-tokenizer = GPT2TokenizerFast(vocab_file='../dataset/BPE/vocab.json'
-                              , merges_file="../dataset/BPE/merges.txt"
-                              )
-
-a = tokenizer.encode("Hello, world!")
-print(a)
